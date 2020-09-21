e0b4defe95e3621aa2acc9e80987658c8229ad76
mikami <mikami@pepabo.com>
2020-09-21 17:25:43 +0900

    helloメソッドを復活させる
    演習で消したやつを復活させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def hello
+    render html: "hello, world!"
+  end
+
   def hola
     render html: "¡hola, mundo!"
   end
